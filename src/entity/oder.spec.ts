c650e9a1a38e91060f4774da8ba4518b449263bd
JhoMartins <jhonatanbmartins@gmail.com>
2024-10-01 22:40:34 -0300

    feat: relating order_item with product

--- src/entity/oder.spec.ts
+++ src/entity/oder.spec.ts
@@ -21,18 +21,25 @@ describe("Oder unit tests", () => {
 	});
 
   it("Should calculate total", () => {
-    const item = new OrderItem("i1", "Item 1", 100);
-    const item2 = new OrderItem("i2", "Item 2", 200);
+    const item = new OrderItem("i1", "Item 1", 100, "p1", 2);
+    const item2 = new OrderItem("i2", "Item 2", 200, "p2", 2);
     const order = new Order("01", "c1", [item]);
 
     var total = order.total();
 
-    expect(total).toBe(100);
+    expect(total).toBe(200);
 
     const order2 = new Order("02", "c2", [item, item2]);
     var total = order2.total();
     
-    expect(total).toBe(300);
+    expect(total).toBe(600);
+	});
+
+  it("Should throw error if the item qtd is less or equal 0", () => {
+    expect(() => {
+      const item = new OrderItem("i1", "Item 1", 100, "p1", 0);
+      const order = new Order("01", "c1", [item]);
+    }).toThrowError("Quantity must be greather than zero");
 	});
 
 });
